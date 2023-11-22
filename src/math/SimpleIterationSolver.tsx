19788318d54dddf68d1e700d2fbd123ace1431c3
Дмитрий Максимов <deemax3x3@mail.ru>
2023-11-23 00:57:58 +0300

    Second Lab. Simple iterations method

--- src/math/SimpleIterationSolver.tsx
+++ src/math/SimpleIterationSolver.tsx
@@ -1,37 +1,52 @@
-import SolverInterface from "./SolverInterface.tsx";
-import MatrixSizeError from "./errors/MatrixSizeError.tsx";
-import NonSquareMatrixError from "./errors/NonSquareMatrixError.tsx";
+import SolverInterface from './SolverInterface';
+import MatrixSizeError from "./errors/MatrixSizeError";
+import NonSquareMatrixError from "./errors/NonSquareMatrixError";
 
 class SimpleIterationSolver implements SolverInterface {
-  solve(matrix: number[][], rows: number, cols: number, freeMembers: number[]): number[] {
-    if (rows < 2 || cols < 2) {
-      throw new MatrixSizeError('Матрица должна быть размером больше чем 1x1');
-    }
+  solve(matrix: number[][], rows: number, cols: number,  freeMembers: number[]): number[] {
+    const epsilon = 0.0001;
+    const maxIterations = 50;
 
     if (rows !== cols) {
       throw new NonSquareMatrixError('Матрица должна быть квадратной');
     }
 
-    let solution: number[] = Array(rows).fill(0);
-    let nextSolution: number[] = Array(rows).fill(0);
-    let error = 1, epsilon = 1e-5;
+    if (rows !== freeMembers.length) {
+      throw new MatrixSizeError('Размеры матрицы и вектора свободных членов не совпадают');
+    }
 
-    while (error > epsilon) {
+    let x: number[] = new Array(cols).fill(0); // начальное приближение
+    let iteration = 0;
+    let converged = false;
+
+    while (iteration < maxIterations && !converged) {
+      const xNew: number[] = new Array(cols).fill(0);
       for (let i = 0; i < rows; i++) {
         let sum = 0;
         for (let j = 0; j < cols; j++) {
           if (i !== j) {
-            sum += matrix[i][j] * solution[j];
+            sum += matrix[i][j] * x[j];
           }
         }
-        nextSolution[i] = (freeMembers[i] - sum) / matrix[i][i];
+        xNew[i] = (freeMembers[i] - sum) / matrix[i][i];  // Итерационная формула для получения нового значения x
       }
-
-      error = Math.max(...solution.map((x, idx) => Math.abs(x - nextSolution[idx])));
-      solution = [...nextSolution];
+      converged = true;
+      for (let i = 0; i < cols; i++) {
+        if (Math.abs(xNew[i] - x[i]) > epsilon) {
+          converged = false;  // Проверка на сходимость по заданной погрешности
+          break;
+        }
+      }
+      x = xNew;  // Обновление значений x для следующей итерации
+      iteration++;
     }
 
-    return solution;
+    if (!converged) {
+      // Обработка случая, когда метод не сошелся за указанное число итераций
+      throw new Error('Метод не сошелся за указанное число итераций');
+    }
+
+    return x;  // Возвращение решения
   }
 }
 
